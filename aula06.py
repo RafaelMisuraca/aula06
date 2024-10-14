4d45cb3ce92dcddb52de52ab7f64ef9184c83069
RafaelMisuraca <rafaelmisuraca@gmail.com>
2024-10-14 18:57:58 -0300

    Add files via upload
    algumas eu não consegui terminar.

---
+++ aula06.py
@@ -0,0 +1,125 @@
+#Atv1
+#unidade = print(input("Digite o valor em metros:"))
+#print(unidade // 100)
+
+
+
+
+
+
+#Atv2
+#base = input("Digite a base do retângulo:")
+#altura = input("Digite a altura do retângulo:")
+#print(base * altura)
+
+
+
+
+
+
+#Atv3
+#peso = float(input("Digite aqui o seu peso:"))
+#altura = float(input("Digite aqui sua altura (em metros):"))
+#altura_total = altura * altura
+#print(peso / altura_total)
+
+
+
+
+
+#Atv4
+#capital = float(input("Digite o capital:"))
+#taxa_de_juros = float(input("Digite a taxa de juros:"))
+#tempo = float(input("Digite o tempo:"))
+#print(capital * taxa_de_juros * tempo)
+
+
+
+
+
+#Atv5
+#a = float(input("Digite uma nota: "))
+#b = float(input("Digite uma segunda nota: "))
+#c = float(input("Digite uma terceira nota: "))
+#d = float(input("Digite uma quarta nota: "))
+#soma = a + b + c + d
+#media = soma / 4
+#print(media)
+#if media >=6:
+#    print("Aprovado")
+#else:
+#    print("Reprovado")
+
+
+
+#Atv7
+#a = float(input("Digite o preço: "))
+#b = float(input("Digite o desconto: "))
+#desconto = b /100
+#valor = a * desconto
+#print(valor)
+#valor_final = a - valor
+#print(valor_final)
+
+
+#Atv8
+#segundos = float(input("Digite o tempo em segundos: "))
+#print(segundos)
+#minutos = segundos / 60
+#print(minutos)
+#horas = minutos / 60
+#print(horas)
+
+
+
+
+#Atv9
+#a = float(input("Digite a temperatura em graus Celsius: "))
+#transformacao = a * 1.8 + 32
+#print(f"A transformação em Fahrenheit é {transformacao}")
+
+
+
+
+
+
+#Atv10
+# a = int(input("Digite sua idade: "))
+# if 0 <= a <= 12:
+#     print("Você é uma criança.")
+# elif 12 < a < 18:
+#     print("Você é um Adolescente.")
+# elif 18 <= a < 65:
+#     print("Você é um adulto.")
+# elif a >= 65:
+#     print("Você é uma pessoa idosa.")
+# else:
+#     print("A idade não existe. Tente novamente.")
+
+
+#Atv11
+#nota = int(input("Digite uma nota de 0 a 100: "))
+#if nota >=90:
+#    print("A")
+#elif nota >=80:
+#    print("B")
+#elif nota >=70:
+#    print("C")
+#elif nota >= 60:
+#    print("D")
+#else:
+#    print("F")
+
+
+
+
+
+
+
+
+#Atv13
+numero = 10
+while numero >= 0:
+    print(numero)
+    numero -= 1
+print("Feliz Ano Novo!")
